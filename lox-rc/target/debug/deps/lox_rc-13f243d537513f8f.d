90ab10ef2fc5f8e23469c2c4e696c21c1ec9b1b5
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-01-23 21:46:46 +0000

    Remove target directory from version control

--- lox-rc/target/debug/deps/lox_rc-13f243d537513f8f.d
+++
@@ -1,5 +0,0 @@
-/home/bpandit/Projects/personal/lox-editor/lox-rc/target/debug/deps/lox_rc-13f243d537513f8f: src/main.rs
-
-/home/bpandit/Projects/personal/lox-editor/lox-rc/target/debug/deps/lox_rc-13f243d537513f8f.d: src/main.rs
-
-src/main.rs:
